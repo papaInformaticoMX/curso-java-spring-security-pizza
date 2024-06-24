10b20627a6f5dd33ee5945da6c4114b9a880a7d9
Papa informatico <papainformaticomx@gmail.com>
2024-06-23 21:39:01 -0600

    FirstCommit

---
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.platzi.pizza.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        // Permite todas las peticiones
+        /*http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();*/
+        return http.build();
+    }
+}
