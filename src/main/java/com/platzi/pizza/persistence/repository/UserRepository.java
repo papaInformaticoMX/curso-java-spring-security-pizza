cd655b9361b5f9f4db4dc50d401a665e4b60d96a
Papa informatico <papainformaticomx@gmail.com>
2024-06-24 15:10:34 -0600

    Adding roles from databases and authorities for partial permissions

--- src/main/java/com/platzi/pizza/persistence/repository/UserRepository.java
+++ src/main/java/com/platzi/pizza/persistence/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.platzi.pizza.persistence.repository;
 
-public class UserRepository {
+import com.platzi.pizza.persistence.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, String> {
 }
--- src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/platzi/pizza/web/config/SecurityConfig.java
@@ -4,13 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
@@ -23,37 +20,18 @@ public class SecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/pizzas/**").hasAnyRole("ADMIN", "CUSTOMER")
                 .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
+                .requestMatchers("/api/orders/random").hasAuthority("random_order")
                 .requestMatchers("/api/orders/**").hasRole("ADMIN")
+
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
-        // Permite todas las peticiones
-        /*http
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll();*/
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService memoryUsers(){
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder().encode("admin"))
-                .roles("ADMIN")
-                .build();
-
-        UserDetails customer = User.builder()
-                .username("customer")
-                .password(passwordEncoder().encode("customer123"))
-                .roles("CUSTOMER")
-                .build();
 
 
-        return  new InMemoryUserDetailsManager(admin, customer);
-
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
